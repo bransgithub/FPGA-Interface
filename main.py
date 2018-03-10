daf9e2334b17393d4d8c169f72615baaa11d7be6
branch331 <34431991+branch331@users.noreply.github.com>
2018-03-09 19:21:34 -0600

    implemented basic file writing, and set up program for basic data logging with FPGA

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from nifpga import Session
 bitfilepath = "mybitfile.lvbitx"
 targetname = "RIO0"
 
-indicators = [] #Initialize an empty list of indicators to reference
+#indicators = [] #Initialize an empty list of indicators to reference
+
+log_file = open("log.txt", "w")
+
+num_data_points = 10000 #Set the number of data points to log for
 
 with Session(bitfile = bitfilepath, resource = targetname) as session:
 	session.reset() #Stop FPGA logic and puts it into the default state
@@ -12,4 +16,9 @@ with Session(bitfile = bitfilepath, resource = targetname) as session:
 	
 	#Data acquisition here
 	
-	#Log data here
+	for i in range(num_data_points):
+		my_indicator = session.registers['My Indicator']
+		
+		#Log data here		
+		log_file.write("{0:.4f}\n".format(my_indicator))
+		#print(my_indicator)
